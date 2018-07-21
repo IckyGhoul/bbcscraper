dbbfe2c48ff6b2c4436463390ad872484ac54b07
IckyGhoul <33111465+IckyGhoul@users.noreply.github.com>
2018-07-20 22:08:07 -0600

    note correction

--- BBCscrape/public/app.js
+++ BBCscrape/public/app.js
@@ -33,7 +33,7 @@ $.getJSON("/articles", function(data) {
         // The title of the article
         $("#notes").append("<h4>" + data.title + "</h4>");
         // An input to enter a new title
-        $("#notes").append("<input id='titleinput'' name='title' >");
+       
         // A textarea to add a new note body
         $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
         // A button to submit a new note, with the id of the article saved to it
